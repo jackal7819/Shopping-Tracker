655cc8162804a436c86c92161db248d1d9e41ef6
“jackal7819” <jackal3166@gmail.com>
2023-04-18 20:53:08 +0300

    Create ExpensesList

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+    {
+        id: 'iq1',
+        title: 'Steam Deck',
+        amount: 399.69,
+        date: new Date(2023, 1, 11),
+    },
+    {
+        id: 'iq2',
+        title: 'Nintendo Switch',
+        amount: 299.19,
+        date: new Date(2022, 7, 15),
+    },
+    {
+        id: 'iq3',
+        title: 'PlayStation 5',
+        amount: 499.39,
+        date: new Date(2021, 4, 9),
+    },
+    {
+        id: 'iq4',
+        title: 'LG Oled TV',
+        amount: 857.99,
+        date: new Date(2020, 3, 28),
+    },
+    {
+        id: 'iq5',
+        title: 'Lenovo Laptop',
+        amount: 967.49,
+        date: new Date(2020, 7, 13),
+    },
+];
+
 const App = () => {
-    const expenses = [
-        { date: new Date(2020, 3, 28), title: 'New TV', amount: 857.99 },
-        { date: new Date(2021, 4, 9), title: 'PlayStation 5', amount: 499.39 },
-        { date: new Date(2022, 7, 15), title: 'XBox Series S', amount: 299.19 },
-        { date: new Date(2023, 1, 11), title: 'Steam Deck', amount: 399.69 },
-    ];
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
     const addExpenseHandler = (expense) => {
-        console.log(expense);
+        setExpenses((prevExpenses) => [expense, ...prevExpenses]);
     };
 
     return (
